da98085091032b954167460b815a66dce713283a
Nicholas <nics-lohr@gmx.de>
2019-05-07 20:25:33 +0200

    Registered Authorization blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -17,4 +17,7 @@ def build_app(conf: object = Config) -> Flask:
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
     return app
