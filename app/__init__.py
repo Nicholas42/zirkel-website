5a084627ff9b76187ab91e34c4939488c9adb65a
Nicholas <nics-lohr@gmx.de>
2019-05-07 19:58:39 +0200

    Added main view so we can test later.

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,7 @@ def build_app(conf: object = Config) -> Flask:
     db.init_app(app)
     migrate.init_app(app, db)
 
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     return app
