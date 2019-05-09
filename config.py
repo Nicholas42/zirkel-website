b60f4e61d2cad23c591fafc129c68878bf40feb4
Nicholas <nics-lohr@gmx.de>
2019-05-09 20:20:36 +0200

    SECRET_KEY can now be set by environment.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
-    SECRET_KEY = "Manfs größtes Gehemnis"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "Manfs größtes Gehemnis"
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
