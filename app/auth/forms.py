f497cdd08b7129bb22a0c41969e1c966f2bd0857
Nicholas <nics-lohr@gmx.de>
2019-05-10 00:15:23 +0200

    Added change password functionality.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
@@ -22,11 +23,22 @@ class LoginForm(FlaskForm):
         yield self.password
 
 
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField(label="Altes Passwort", validators=[DataRequired()])
+    npassword = PasswordField(label="Neues Passwort", validators=[DataRequired()])
+    npassword1 = PasswordField(label="Neues Passwort wiederholen", validators=[DataRequired(), EqualTo('npassword')])
+    submit = SubmitField(label="Ändern")
+
+    def validate_password(self, password):
+        return current_user.check_password(password.data)
+
+
 class RegistrationForm(FlaskForm):
     username = StringField(label="Benutzername",
                            validators=[DataRequired(), validate_unique("username", "Benutzername bereits vergeben,")])
     email = StringField(label="E-Mail",
-                        validators=[DataRequired(), Email(), validate_unique("email", "E-Mail-Adresse bereits benutzt.")])
+                        validators=[DataRequired(), Email(),
+                                    validate_unique("email", "E-Mail-Adresse bereits benutzt.")])
     password = PasswordField(label="Passwort", validators=[DataRequired()])
     password2 = PasswordField(label="Wiederhole Passwort", validators=[DataRequired(), EqualTo('password')])
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,8 @@
 from flask import redirect, url_for, request, flash, render_template
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from app import db
 from app.auth import bp
-from app.auth.forms import LoginForm, RegistrationForm
+from app.auth.forms import LoginForm, RegistrationForm, ChangePasswordForm
 from app.helpers.route_helpers import safe_next
 from app.models import User
 
@@ -31,6 +31,7 @@ def login():
 
 
 @bp.route("/logout")
+@login_required
 def logout():
     logout_user()
     flash("Abgemeldet.", category="success")
@@ -51,7 +52,23 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        flash("Du bist jetzt registriert, %s!" % user.username)
+        flash("Du bist jetzt registriert, %s!" % user.username, "success")
         return redirect(url_for("auth.login"))
 
     return render_template("auth/register.html", form=form, title="Registrierung")
+
+
+@bp.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        current_user.set_password(form.npassword.data)
+
+        db.session.commit()
+
+        flash("Passwort erfolgreich geändert.", "success")
+
+        return redirect(url_for("main.index"))
+
+    return render_template("auth/change_password.html", form=form, title="Passwort ändern")
