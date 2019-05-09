ef1aa5274c99a8c2c120b773042a72c647f3d3f1
Nicholas <nics-lohr@gmx.de>
2019-05-09 21:46:55 +0200

    Changed port.

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --name zirkel -d -p 8000:5000 --restart always --env-file .env \
+docker run --name zirkel -d -p 9000:5000 --restart always --env-file .env \
     --link mysql:dbserver \
     zirkel:latest
