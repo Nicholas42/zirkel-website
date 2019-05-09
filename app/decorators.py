346284ab06b09f34e6bdca736878e35cdd33eaf0
Nicholas <nics-lohr@gmx.de>
2019-05-09 22:52:55 +0200

    Simplified role_required decorator.

--- app/decorators.py
+++ app/decorators.py
@@ -1,16 +1,12 @@
 from flask import abort
 from flask_login import current_user
-from app import db
-from app.models import Role, user_roles
+from app.models import Role
 
 
 def role_required(role):
     def decorator(f):
         def decorated_function(*args, **kwargs):
-            user_id = current_user.id
-            role_id = Role.query.filter_by(name=role).first().id
-
-            if db.session.query(user_roles).filter_by(user_id=user_id, role_id=role_id).first() is None:
+            if not Role.query.filter_by(name=role).first() in current_user.roles:
                 abort(403)
 
             return f(*args, **kwargs)
