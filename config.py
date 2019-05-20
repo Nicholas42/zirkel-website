9a88748dcd57ad648b307b8b89e13c598feb26f5
Nicholas <nics-lohr@gmx.de>
2019-05-21 01:16:08 +0200

    Improved mail config.

--- config.py
+++ config.py
@@ -22,10 +22,9 @@ class Config:
     ADMIN_MAIL = os.environ.get('ADMIN_MAIL') or "root@localhost"
     ADMIN_PW = os.environ.get('ADMIN_PW')
 
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_SERVER = os.environ.get("MAIL_SERVER") or "localhost"
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 0)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") or ""
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or ""
 
