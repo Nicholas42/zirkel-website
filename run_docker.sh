c28de19156ffad0e6f168f3d86dac8b4d1fdbfa6
Nicholas <nics-lohr@gmx.de>
2019-05-22 15:46:03 +0200

    Docker: Added volume for modules.

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,5 @@
 
 docker run --name zirkel -d -p 9000:5000 --restart always --env-file .env \
     --link postgres_db:dbserver \
+    --mount source=zirkel-modules,target=/home/zirkel/.data \
     zirkel:latest
