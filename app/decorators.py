a52710059076eb6f485a97d297f34088b672085d
Nicholas <nics-lohr@gmx.de>
2019-05-23 00:13:16 +0200

    Added wraps to decorators so endpoints work.

--- app/decorators.py
+++ app/decorators.py
@@ -1,9 +1,12 @@
+from functools import wraps
+
 from flask import abort, render_template
 from flask_login import current_user, login_required
 
 
 def role_required(role):
     def decorator(f):
+        @wraps(f)
         @login_required
         def decorated_function(*args, **kwargs):
             if not current_user.has_role(role):
@@ -17,6 +20,7 @@ def role_required(role):
 
 
 def disable_route(f):
+    @wraps(f)
     def decorate_function(*args, **kwargs):
         return render_template("page_disabled.html"), 403
 
